0463f7bc96b1ece1c4dfbc7eb899db0a885b9ead
AsiaKo <asiakozik@gmail.com>
2018-05-09 14:03:03 -0400

    Expires Headers

--- js/main.js
+++ js/main.js
@@ -2,25 +2,21 @@
 /* ==========================================================================
   MAIN.JS
    ========================================================================== */
-'use strict';
-
- 
-
-
+"use strict";
 
 function initMap() {
 
         var kldny = {lat: 40.7429012, lng: -73.98413819999996};
-        var map = new google.maps.Map(document.getElementById('map'), {
+        var map = new google.maps.Map(document.getElementById("map"), {
             zoom: 16,
-        streetViewControl: false, 
+            streetViewControl:false, 
             center: kldny,
-          styles: [{
-            featureType: '',
-            elementType: 'all',
-            stylers: [ 
-              { "saturation": -100}, 
-              { "gamma": 0.95} 
+            styles: [{
+            featureType: "",
+            elementType:"all",
+            stylers:[ 
+              {"saturation": -100}, 
+              {"gamma": 0.95} 
               ]
           }]
         });
@@ -28,16 +24,16 @@ function initMap() {
         var marker = new google.maps.Marker({
             position: kldny,
             map: map,
-            title: 'KLD NY', 
-            icon: "https://github.com/AsiaKo/KLD-NY/blob/gh-pages/assets/icons/location-color.png?raw=true"
+            title:"KLD NY", 
+            icon:"https://github.com/AsiaKo/KLD-NY/blob/gh-pages/assets/icons/location-color.png?raw=true"
          });
-    };
+    }
 
 
 
 $(document).ready(function(){
 
-   var scrollLink = $('.scroll');
+   var scrollLink = $(".scroll");
   // Smooth scrolling
     scrollLink.click(function(e) {
     if (this.hash !== ""){
@@ -46,7 +42,7 @@ $(document).ready(function(){
          var target = this.hash;
          var targetOffset = $(target).offset().top - 50;
 
-        $('body,html').animate({
+        $("body,html").animate({
           scrollTop: targetOffset
         }, 1000,  function(){
           window.location.hash = targetOffset;
@@ -57,30 +53,30 @@ $(document).ready(function(){
 
 	// BURGER MENU/ OVERLAY NAV OPEN
 
-  $("#nav-burger").on('click', function(e) {
+  $("#nav-burger").on("click", function(e) {
       e.preventDefault();
-    $(this).toggleClass('open');
-    console.log('overlay open');
+    $(this).toggleClass("open");
+    console.log("overlay open");
     $("header").toggleClass("overlay-open");
-    $(".menu-items").toggleClass('open');
+    $(".menu-items").toggleClass("open");
   });
 
 
-  $('a').on('click', function(){
+  $("a").on("click", function(){
   $("header").removeClass("overlay-open");
-  $(".menu-items, #nav-burger").removeClass('open');
+  $(".menu-items, #nav-burger").removeClass("open");
   });
 
 // -------- END ANIMATIONS ON SCROLL -------------
 
-  $(window).on('scroll', function() {
+  $(window).on("scroll", function() {
 
   //  CHANGE NAV HEIGHT AND ADD BOX-SHADOW ON SCROLL
 
         if($(window).scrollTop()> 100)  {
-            $('nav, header,  .nav-toggle').addClass('scroll-nav'); 
+            $("nav, header,  .nav-toggle").addClass("scroll-nav"); 
               }else  {
-              $('nav, header').removeClass('scroll-nav');        
+              $("nav, header").removeClass("scroll-nav");        
               }
           });
 
@@ -91,12 +87,12 @@ $(document).ready(function(){
 
 
 //Cache reference to window and animation items
-var $animation_elements = $('.animate, .portfolio-item');
+var $animation_elements = $(".animate, .portfolio-item");
 var $window = $(window);
 
-$window.on('scroll', check_if_in_view);
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
+$window.on("scroll", check_if_in_view);
+$window.on("scroll resize", check_if_in_view);
+$window.trigger("scroll");
 
 function check_if_in_view() {
   var window_height = $window.height();
@@ -112,31 +108,24 @@ function check_if_in_view() {
     //check to see if this current container is within viewport
     if ((element_bottom_position >= window_top_position) &&
         (element_top_position <= window_bottom_position)) {
-         $element.addClass('fade-up');
+         $element.addClass("fade-up");
        } 
   });
 }
-
-
-  
-  // --------SCROLL DOWN -------------  
-
-
+ 
+  // -------- SCROLL DOWN -------------  
     $(window).scroll(function(){
             if ($(this).scrollTop() > 200) {
-                $('.line-scroll').fadeOut(400);
+                $(".line-scroll").fadeOut(400);
             } 
         });
 
-
-  // --------BACK TO TOP -------------  
-
-
+  // -------- BACK TO TOP -------------  
      $(window).scroll(function(){
             if ($(this).scrollTop() > 600) {
-                $('.line-top').css({"opacity": "1", "transform": "translate3d(0,0,0)"});     
+                $(".line-top").css({"opacity": "1", "transform": "translate3d(0,0,0)"});     
             } else {
-                $('.line-top').css({"opacity": "0", "transform": "translate3d(0,30px,100px)"});
+                $(".line-top").css({"opacity": "0", "transform": "translate3d(0,30px,100px)"});
             }
         });
        
@@ -144,47 +133,58 @@ function check_if_in_view() {
 
  //Click event to scroll to top
 
-    $('.line-top').click(function(){
-        $('html, body').animate({scrollTop : 0},700);
-        $('.line-top').css({"opacity": "0"});
-        return false;
-   
+    $(".line-top").click(function(){
+        $("html, body").animate({scrollTop : 0},700);
+        $(".line-top").css({"opacity": "0"});
+        return false;   
 });  
 
     
   
   // --------LIGHTSLIDER -------------  
 
-// $(document).ready(function() {    
-//   $('#lightSlider').lightSlider({
-//     item:1,
-//     loop: true,
-//     controls: false,
-//     speed: 2000,
-//     auto: true,
-//     pause: 6000,
-//     pauseOnHover: true,
-//     mode: 'slide',
-//   });
+$(document).ready(function() {    
+  $('#lightSlider').lightSlider({
+    item:1,
+    loop: true,
+    controls: false,
+    speed: 2000,
+    auto: true,
+    pause: 6000,
+    pauseOnHover: true,
+    mode: 'slide',
+  });
 
-// });
+});
+
 
 
   // --------LIGHTGALLERY -------------  
 
-// $(document).ready(function(){
+$(document).ready(function(){
 
-//   $('#srcset').lightGallery({
-//     thumbnail:false,
+  $('#srcset').lightGallery({
+    thumbnail:false,
 
-//     showThumbByDefault: false, 
-//     speed: 1000, 
-//     easing: 'ease',
-//   }); 
+    showThumbByDefault: false, 
+    speed: 1000, 
+    easing: 'ease',
+  }); 
 
-// });
+});
 
 
+
+  // -------- RELLAX -------------  
+
+
+let rellax = new Rellax('.rellax', {
+    speed: -2,
+    center: true,
+    round:true,
+    vertical: true,
+    horizontal: false
+  });
 
 
 
