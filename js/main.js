ddde776a523b6400ba3634e63f5948d7a879e5ad
AsiaKo <asiakozik@gmail.com>
2018-08-24 12:40:40 -0400

    sw

--- js/main.js
+++ js/main.js
@@ -4,6 +4,16 @@
    ========================================================================== */
 "use strict";
 
+// Service Worker 
+if('serviceWorker'in navigator) {
+  window.addEventListener('load', () =>{
+    navigator.serviceWorker
+      .register('../sw.js')
+      .then(reg => console.log('Service Worker Registered'))
+      .catch(err => console.log(`Service Worker: Error: ${err}`))
+  })
+}
+
 
 $(document).ready(function(){
 
--- sw.js
+++ sw.js
@@ -1,18 +1,50 @@
-//importScripts('/cache-polyfill.js');
+const cacheName = 'v1.0';
 
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
-     return cache.addAll([
-       '/',
-       '/index.html',
-       '/gallery.html',
-       '/index.html?homescreen=1',
-       '/?homescreen=1',
-       '/css/main.css',
-       '/js/main.js',
-       '/img/**'
-     ]);
-   })
- );
+const cacheAssets = [
+	'index.html',
+	'gallery.html',
+	'/css/main.css',
+	'/js/main.js'
+];
+
+
+// Call Install Event
+self.addEventListener('install', e => {
+  console.log('Service Worker: Installed');
+
+  e.waitUntil(
+	  	caches
+	  	 .open(cacheName)
+	  	 .then(cache => {
+	  	 	console.log('Service Worker: Caching Files');
+	  	 	cache.addAll(cacheAssets);
+	  	})
+	  	 .then(() => self.skipWaiting())
+  	);
 });
+
+// Call Activate Event
+self.addEventListener('activate', e => {
+  console.log('Service Worker: Activated');
+  //Remove unwanted caches
+  e.waitUntil(
+  		caches.keys().then(cacheNames => {
+  			return Promise.all(
+  				cacheNames.map(cache => {
+  					if(cache !== cacheName){
+  					console.log('Service Worker: Clearing Old Cache');
+  					return caches.delete(cache);
+  					}
+  				})
+  			)
+  		})
+  	);
+});
+
+// Call Fetch Event
+self.addEventListener('fetch', e => {
+	console.log('Service Worker: Fetching');
+	e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
+
+
