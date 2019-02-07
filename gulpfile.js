fca472b8136308c4252a9338b0fa89c55e861822
AsiaKo <asiakozik@gmail.com>
2019-02-06 20:45:07 -0500

    fixed gulpfile - uglify

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,57 @@ const rename = require('gulp-rename');
 const babel = require('gulp-babel');
 const htmlreplace = require('gulp-html-replace');
 const imagemin = require('gulp-imagemin');
+var pump = require('pump');
+
+
+
+
+
+gulp.task('replacePath', done => {
+  gulp.src('src/*.html')
+    .pipe(htmlreplace({
+        'css': 'css/main.min.css',
+        'js': 'js/main.min.js'
+    }))
+    .pipe(gulp.dest('dist/'));
+
+     done();
+});
+
+
+//CONCAT AND MINIFY CSS
+gulp.task('styles', done => {
+	gulp.src('src/css/*.css')
+	  .pipe(concat('main.css'))
+	  .pipe(cssmin())
+	  .pipe(rename({
+	    suffix: '.min', 
+	  }))
+	  .pipe(gulp.dest('dist/css'));
+
+	  done();
+});
+
+
+// COMPILE and MINIFY JS MAIN
+gulp.task('scripts', () =>
+   gulp.src('src/js/*.js')
+      .pipe(babel({
+            presets: ['@babel/env']
+        }))
+      .pipe(uglify())
+	  .pipe(rename({
+	    suffix: '.min'
+	  }))
+      .pipe(gulp.dest('dist/js/'))
+);
+//Optimize Images
+gulp.task('images', done =>{
+	gulp.src('src/img/**/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/img'));
+	  done();	
+});
 
 
 
@@ -31,6 +82,7 @@ gulp.task('copy', done => {
 	 done();
 });
 
+//REPLACE CSS AND JS FILE PATHS IN HTML 
 
 gulp.task('replacePath', done => {
   gulp.src('src/*.html')
@@ -39,82 +91,27 @@ gulp.task('replacePath', done => {
         'js': 'js/main.min.js'
     }))
     .pipe(gulp.dest('dist/'));
-
      done();
 });
 
-//MINIFY VENDOR CSS
-// gulp.task('stylesVendor', done => {
-// 	gulp.src('src/css/vendor/lightgallery.css')
-// 	  .pipe(cssmin())
-// 	  .pipe(rename({
-// 	    suffix: '.min', 
-// 	  }))
-// 	  .pipe(gulp.dest('dist/css/vendor'));
+gulp.task('default', gulp.series('styles','scripts','images', 'replacePath','copy')); 
 
-// 	  done();
-// });
-
-
-//CONCAT AND MINIFY CSS
-gulp.task('styles', done => {
-	gulp.src('src/css/*.css')
-	  .pipe(concat('main.css'))
-	  .pipe(cssmin())
-	  .pipe(rename({
-	    suffix: '.min', 
-	  }))
-	  .pipe(gulp.dest('dist/css'));
-
-	  done();
+gulp.task('watch', function(){
+	gulp.watch('src/js/*.js', gulp.series('scripts'));
+	gulp.watch('src/css/*.css', gulp.series('styles'));
+	gulp.watch('src/img/**/*', gulp.series('images'));
+	gulp.watch(['src/css/vendor/*',
+				'src/css/fonts/*', 
+				'src/assets/**/*', 
+				'src/404.html', 
+				'src/index.html', 
+				'src/gallery.html', 
+				'src/manifest.json',
+				'src/sw.js',
+				'src/browserconfig.xml',
+				'src/.htaccess', 
+				'src/js/vendor/*' ], gulp.series('copy'));
 });
-
-
-// COMPILE and MINIFY JS MAIN
-gulp.task('scripts', done =>{
-	gulp.src('src/js/*.js')
-	  .pipe(babel({
-	      presets: ['@babel/preset-env']
-	  }))
-	  .pipe(uglify())
-	  .pipe(rename({
-	    suffix: '.min'
-	  }))
-	  .pipe(gulp.dest('dist/js/'));
-
-	   done();
-});
-
-//Optimize Images
-gulp.task('images', done =>{
-	gulp.src('src/img/**/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('dist/img'));
-	  done();	
-});
-
-//REPLACE CSS AND JS FILE PATHS IN HTML 
-
-gulp.task('replacePath', done => {
-  gulp.src('dist/*.html')
-    .pipe(htmlreplace({
-        'css': 'css/main.min.css',
-        'js': 'js/main.min.js'
-    }))
-    .pipe(gulp.dest('dist/'));
-     done();
-});
-
-
-gulp.task('default', gulp.parallel('copy','styles','scripts','images', 'replacePath')); 
-// gulp.task('default', gulp.series(gulp.parallel('copy','styles','scripts','images'), 'replacePath')); 
-
- // gulp.task('default', done => {
- // 	gulp.parallel('copy','styles','scripts','images', 'replacePath');
- // 	done();
- // }); 
-
-
 
 
 
