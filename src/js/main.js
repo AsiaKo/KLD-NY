e32313e8700945b5d080571cc416f16692176c60
AsiaKo <asiakozik@gmail.com>
2019-03-03 17:54:10 -0500

    .gitignore update

--- src/js/main.js
+++ src/js/main.js
@@ -52,8 +52,7 @@ $(document).ready(function(){
   $("#nav-burger").on("click", function(e) {
       e.preventDefault();
     $("header").toggleClass("overlay-open");
-    $(".menu-items, #nav-burger").toggleClass("open");
-
+    $(".menu-items, #nav-burger").toggleClass("open")
   });
 
   $("a").on("click", function(){
@@ -177,4 +176,36 @@ function initMap() {
          });
     }
 
-//
+
+
+function google_maps_lazyload(api_key) {
+  'use strict'
+
+  if (api_key) {
+    var options = {
+      rootMargin: '100px',
+      threshold: 0
+    }
+
+    var map = document.getElementById('map')
+
+    var observer = new IntersectionObserver(
+      function(entries, self) {
+        
+        var isIntersecting = typeof entries[0].isIntersecting === 'boolean' ? entries[0].isIntersecting : entries[0].intersectionRatio > 0
+        if (isIntersecting) {
+          var mapsJS = document.createElement('script');
+          mapsJS.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&key=' + api_key;
+          document.getElementsByTagName('head')[0].appendChild(mapsJS);
+          self.unobserve(map);
+        }
+      },
+      options
+    )
+
+    observer.observe(map)
+  }
+}
+
+google_maps_lazyload("AIzaSyB6VXCVtjWl8I7ZnqLSHxIWOaFqEMVskmc");
+
